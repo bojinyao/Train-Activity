d10bb6a35c85dfc9f7a19737a3ce189368d08ab3
Max Yao <bojinyao@berkeley.edu>
2018-07-03 20:52:03 -0700

    Ready to incorporate js file

---
+++ assets/javascript/app.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyDBy4ZiUXiC_jixXM4kS44zjiTmBMx0CjU",
+        authDomain: "train-time-log.firebaseapp.com",
+        databaseURL: "https://train-time-log.firebaseio.com",
+        projectId: "train-time-log",
+        storageBucket: "train-time-log.appspot.com",
+        messagingSenderId: "729994678291"
+    };
+    firebase.initializeApp(config);
+
+    
+
+
+
+})
